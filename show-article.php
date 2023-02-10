78fa469f2cf7fadef6fbcdef40f657bf7c0a5a66
Albaficapis <albaficapisukesu@mail.fr>
2023-02-10 17:01:02 +0100

    Mise en place de la POO et de la classe articleDB pour gérer le CRUD

--- show-article.php
+++ show-article.php
@@ -1,17 +1,13 @@
 <?php
-$filename = __DIR__.'/data/articles.json';
-$articles = [];
+$articleDB = require_once './database/models/ArticleDB.php';
+$statement = $pdo->prepare('SELECT * FROM article WHERE id=:id');
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $id = $_GET['id'] ?? '';
 
 if(!$id) {
     header('Location: /');
 } else {
-    if(file_exists($filename)) {
-        $articles = json_decode(file_get_contents($filename), true) ?? [];
-        $articleIndex = array_search($id, array_column($articles, 'id'));
-        $article = $articles[$articleIndex];
-    }
+    $article = $articleDB->fetchOne($id);
 }
 ?>
 <!DOCTYPE html>
